00490fc7a906c8d18c80a9e7788e96d1a9fbaa7a
chescoreloaded <ingfranciscoroldan@gmail.com>
2020-08-23 19:35:06 -0400

    tercera unidad

--- src/App.js
+++ src/App.js
@@ -21,18 +21,65 @@ const list = [
   },
 ];
 
+function isSearched(searchTerm){
+  return function(item){
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 
 class App extends Component {
-  render(){
 
+constructor(props){
+  super(props);
+  this.state = {
+    list,
+    searchTerm: '',
+  };
+
+  this.onDismiss = this.onDismiss.bind(this);
+  this.onSearchChange = this.onSearchChange.bind(this);
+}
+
+
+
+onDismiss(id){
+  
+  const isNotId =  item=>item.objectID !== id;
+  const updatedList = this.state.list.filter(isNotId);
+  this.setState({list:updatedList});
+}
+
+onSearchChange(event){
+  this.setState({searchTerm: event.target.value});
+}
+
+  render(){
     return (
+
       <div className="App">
-        {list.map(item =>
+      <form>
+        <input type="text"
+               onChange={this.onSearchChange}
+
+
+        />
+
+      </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>{ <a href={item.url}>{item.title}</a> }</span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+                <button 
+                onClick={()=>this.onDismiss(item.objectID)}
+                type="button"
+                >
+                Dismiss
+                </button>
+            </span>            
           </div>  
         )}
       </div>
